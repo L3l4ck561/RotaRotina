fc4734c07a168acf925ed0308f9ca6b218543472
L3l4ck561 <carlos.gabriel561btu@gmail.com>
2026-03-15 23:28:24 -0300

    evitando valores booleanos no localstorage

--- Application/src/components/backup/config.js
+++ Application/src/components/backup/config.js
@@ -2,8 +2,10 @@ export const backup = async () => {
     let valuePaste = prompt("Cole suas tarefas aqui, ou, salve colando em outro arquivo:");
 
     if (valuePaste !== null && valuePaste !== "") {
+        if(valuePaste == 'false' || valuePaste == 'true')return;
         localStorage.setItem("tarefas", valuePaste);
         location.reload();
+
     } else {
         let valeuCopy = localStorage.getItem("tarefas");
 
