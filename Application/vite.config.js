918a6486f8426d9943d4a7e1c1d28db5e88d74f7
l3L4ck561 <ti.carlos.dev@gmail.com>
2026-09-10 18:33:28 -0300

    criando woker para baixar o app uma vez - test.01

--- Application/vite.config.js
+++ Application/vite.config.js
@@ -1,11 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-    base:"/"
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+
+      manifest: {
+        name: 'Minha Agenda',
+        short_name: 'Agenda',
+        description: 'Minha agenda pessoal',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  base: "/"
 })
